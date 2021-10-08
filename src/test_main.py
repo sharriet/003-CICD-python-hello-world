966b01ff3930880a28311365729b8a16d87f41bc
Andrew Gibson <andrew.gibson@armakuni.com>
2021-10-08 11:04:09 +0100

    fix test

--- src/test_main.py
+++ src/test_main.py
@@ -15,4 +15,4 @@ def test_hello_world_message_from_index():
 
 def test_hello_world_message_from_client(client):
     response = client.get('/')
-    assert response.data.decode('UTF-8') == 'Hello from main'
+    assert response.data.decode('UTF-8') == 'Hello world'
